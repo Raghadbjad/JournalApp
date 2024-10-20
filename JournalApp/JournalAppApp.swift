7098bdc8e9e2f08c23f6c8ec117a0a12f7d5c643
raghad <raghad.bjaad@gmail.com>
2024-10-20 16:44:24 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Raghad on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalAppTests/JournalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppTests.swift
+//  JournalAppTests
+//
+//  Created by Raghad on 17/04/1446 AH.
+//
+
+import Testing
+@testable import JournalApp
+
+struct JournalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
